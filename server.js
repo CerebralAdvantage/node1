bd680c402c87c66af1de693f3c7f76e64f79ddd5
Jim Huddle <james.r.huddle@gmail.com>
2021-09-30 09:12:38 -0400

    added fileInit() to manage data

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const http = require('http')
 const fs = require('fs')
 
+const fileInit = (fileName, initVal) => {
+  let retVal;
+  if(fs.existsSync(fileName)) {
+    retVal = JSON.parse(fs.readFileSync(fileName, "utf8"));
+  } else {
+    fs.writeFileSync(fileName, JSON.stringify(initVal));
+    retVal = initVal;
+  }
+  return retVal;
+}
+
+let nameList = fileInit("names.txt", []);
+let ID = fileInit("IDs.txt", 0);
+
+
 const server = http.createServer((req, res) => {
   res.end("<h1>Hello</h1>");
 });
