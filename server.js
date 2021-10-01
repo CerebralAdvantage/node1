7753c6662580c1b98437cc1572028e2fdc7280fb
Jim Huddle <james.r.huddle@gmail.com>
2021-10-01 11:09:53 -0400

    working with req.url path data in POST section

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
 const http = require('http')
-const { fileInit, getID, nameList, getPostData } = require('./utils')
+const { getPath, getID, nameList, getPostData } = require('./utils')
 
 const server = http.createServer(async (req, res) => {
-  let path = req.url;
+
+  let path = getPath(req);
 
   if (req.method.toUpperCase() === "GET") {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(nameList));
-  }
+  } // method === "GET"
 
   if (req.method.toUpperCase() === "POST") {
 
     let body = await getPostData(req);
 
-//check url
-
-    res.setHeader('Content-Type', 'text/html');
-    res.end("<h1>Post.</h1>");
-  }
+    if(path[0] && (path[0].toLowerCase() === "items")) { // replace all
+      res.setHeader('Content-Type', 'text/html');
+      res.end("<h1>Post All.</h1>");
+    } else {
+      res.setHeader('Content-Type', 'text/html'); // add one
+      res.end("<h1>Post One.</h1>");
+    }
+  } // method === "POST"
 
   if (req.method.toUpperCase() === "DELETE") {
     res.setHeader('Content-Type', 'text/html');
     res.end("<h1>Delete.</h1>");
-  }
+  } // method === "DELETE"
 
 });
 
