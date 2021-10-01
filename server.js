ce415c6d9e4e58d0722091e20e1e2ff79cc87aea
Jim Huddle <james.r.huddle@gmail.com>
2021-10-01 05:48:00 -0400

    grabbed port from process.env, DELETE works from VS code

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 const fileInit = (fileName, initVal) => {
   let retVal;
-  if(fs.existsSync(fileName)) {
+  if (fs.existsSync(fileName)) {
     retVal = JSON.parse(fs.readFileSync(fileName, "utf8"));
   } else {
     fs.writeFileSync(fileName, JSON.stringify(initVal));
@@ -13,7 +13,7 @@ const fileInit = (fileName, initVal) => {
 }
 
 const getID = () => {
-  let ID = JSON.parse(fs.readFileSync("IDs.txt","utf8"));
+  let ID = JSON.parse(fs.readFileSync("IDs.txt", "utf8"));
   ID++;
   fs.writeFileSync("IDs.txt", JSON.stringify(ID));
   return ID;
@@ -23,14 +23,24 @@ let nameList = fileInit("names.txt", []);
 let ID = fileInit("IDs.txt", 0);
 
 const server = http.createServer((req, res) => {
-
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(nameList));
+  if (req.method.toUpperCase() === "GET") {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(nameList));
+  }
+  if (req.method.toUpperCase() === "POST") {
+    res.setHeader('Content-Type', 'text/html');
+    res.end("<h1>Post.</h1>");
+  }
+  if (req.method.toUpperCase() === "DELETE") {
+    res.setHeader('Content-Type', 'text/html');
+    res.end("<h1>Delete.</h1>");
+  }
 
 });
 
-server.listen(3000,() => {
-  console.log("listening on port 3000");
+let PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 })
 
 
